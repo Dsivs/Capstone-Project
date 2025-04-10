ceac92eb70d89d35e7c83f80768dbc1430b6e22c
Helen Ma <helenma2020@gmail.com>
2025-04-09 20:07:09 -0400

    Update randomforest.py

--- randomforest.py
+++ randomforest.py
@@ -1,5 +1,4 @@
-from Jeff import preprocess # change when we have new feature engineering file
-from Jeff import invoice_generator
+
 
 #import libraries for randon forest
 from sklearn.ensemble import RandomForestClassifier
@@ -8,26 +7,26 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 
 # Load data
-invoices = invoice_generator.generate_dataset() 
-data = preprocess.process_invoice(invoices)
+df_train = pd.read_parquet('train_df.parquet', engine='pyarrow')
+df_test = pd.read_parquet('test_df.parquet', engine='pyarrow')
 
 # Separate features X and target y (anomaly label 0 or 1)
-X = data.drop('label', axis=1)
-y = data['label']
+X_train = df_train.drop('is_anomalous', axis=1)
+y_train = df_train['is_anomalous']
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_test = df_test.drop('is_anomalous', axis=1)
+y_test = df_test['is_anomalous']
 
-# Create a Random Forest Classifier model
+# Create a Random Forest Classifier model 
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-# Train the model
-model.fit(X_train, y_train)
+# Train the model ### NEED TO FIX HERE AND BELOW
+#model.fit(X_train, y_train)
 
 # Make predictions on the test set
-y_pred = model.predict(X_test)
+#y_pred = model.predict(X_test)
 
 # Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
+#accuracy = accuracy_score(y_test, y_pred)
+#print(f"Accuracy: {accuracy}")
 
