5bbc2f1eefddf08cbf2566bdc811381f1d9431cc
Helen Ma <helenma2020@gmail.com>
2025-04-17 18:27:51 -0400

    Update model_app.py

--- model_app.py
+++ model_app.py
@@ -1,11 +1,14 @@
-# Application for Anomaly Detection Model
+"""
+Application for Anomaly Detection Model
 
-# This file takes a dataset (a set of invoices) in json format and outputs which 
-# invoices are anomalous. The json may be the output from Itemize's API.
+This file takes a dataset (a set of invoices) in json format and outputs which 
+invoices are anomalous. The json may be the output from Itemize's API.
 
-# This file imports all the scripts needed. Make sure all scripts are in the same folder.
+This file imports all the scripts needed. Make sure all scripts are in the same folder.
 
-# Date: 4/11/2025
+Date: 4/11/2025
+"""
+
 
 def main():
     # Step 1: Prompt for file path
