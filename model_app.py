facddf0db0a56ea29932830222dd3c1b4aa62c6a
Helen Ma <71906605+Helen029@users.noreply.github.com>
2025-04-17 17:26:52 -0400

    Add files via upload
    application file for final demo

---
+++ model_app.py
@@ -0,0 +1,11 @@
+# Application for Anomaly Detection Model
+
+# This file takes a dataset (a set of invoices) in json format and outputs which 
+# invoices are anomalous. The json may be the output from Itemize's API.
+
+# This file imports all the scripts needed. Make sure all scripts are in the same folder.
+
+# Date: 4/11/2025
+
+import randomforest
+
