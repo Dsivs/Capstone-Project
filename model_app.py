7d5a22ea5823fbe5579c6234955e31e61f328264
Helen Ma <helenma2020@gmail.com>
2025-04-17 18:25:47 -0400

    Update model_app.py

--- model_app.py
+++ model_app.py
@@ -7,5 +7,28 @@
 
 # Date: 4/11/2025
 
-import randomforest
+def main():
+    # Step 1: Prompt for file path
+    json_path = input("Paste the file path for your JSON file: ").strip()
 
+    # Step 2: Run preprocessing
+    print("Running preprocessing...")
+    preprocessed_df = preprocess(json_path)
+
+    # Step 3: Run random forest model
+    print("Running Random Forest...")
+    rf_output_df = run_random_forest(preprocessed_df)
+
+    # Step 4: Run XGBoost model
+    print("Running XGBoost...")
+    anomalies_df = run_xgboost(rf_output_df)
+
+    # Step 5: Print output
+    print("Anomalous invoices detected:")
+    print(anomalies_df)
+
+
+
+
+if __name__ == "__main__":
+    main()
